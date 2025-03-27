d8ac340ff4f5a848a0d74236a7e43a8d457a1c33
opalthailand <opalthailand@gmail.com>
2025-03-27 10:07:39 +0700

    allow anyone to add a collaborator

--- ckanext/cdp/plugin.py
+++ ckanext/cdp/plugin.py
@@ -1,11 +1,99 @@
 # -*- coding: utf-8 -*-
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-# *** เพิ่ม import สำหรับ actions ที่จำเป็น ***
 from ckan import model # Import model เพื่อใช้หา User
-from ckan.logic.action.create import package_collaborator_create
-from ckan.logic.action.delete import package_collaborator_delete
 
+from ckan.types.logic import ActionResult
+import ckan.lib.dictization.model_dictize as model_dictize
+from ckan.types import Context, DataDict, ErrorDict, Schema
+import ckan.logic as logic
+import datetime
+import logging
+from typing import Any
+from flask_babel import gettext as flask_ugettext
+
+log = logging.getLogger(__name__)
+ValidationError = logic.ValidationError
+NotFound = logic.NotFound
+_get_or_bust = logic.get_or_bust
+
+def ugettext(*args: Any, **kwargs: Any) -> str:
+    return flask_ugettext(*args, **kwargs)
+
+_ = ugettext
+
+def package_collaborator_create_any(
+        context: Context,
+        data_dict: DataDict) -> ActionResult.PackageCollaboratorCreate:
+    '''Allow anyone to make a user a collaborator in a dataset.
+    '''
+
+    model = context['model']
+
+    package_id, user_id, capacity = _get_or_bust(
+        data_dict,
+        ['id', 'user_id', 'capacity']
+    )
+
+    package = model.Package.get(package_id)
+    if not package:
+        raise NotFound(_('Dataset not found'))
+
+    user = model.User.get(user_id)
+    if not user:
+        raise NotFound(_('User not found'))
+
+    # Check if collaborator already exists
+    collaborator = model.Session.query(model.PackageMember). \
+        filter(model.PackageMember.package_id == package.id). \
+        filter(model.PackageMember.user_id == user.id).one_or_none()
+    if not collaborator:
+        collaborator = model.PackageMember(
+            package_id=package.id,
+            user_id=user.id)
+    collaborator.capacity = capacity
+    collaborator.modified = datetime.datetime.utcnow()
+    model.Session.add(collaborator)
+    model.repo.commit()
+
+    log.info('User {} added as collaborator in package {} ({})'.format(
+        user.name, package.id, capacity))
+
+    return model_dictize.member_dictize(collaborator, context)
+
+
+def package_collaborator_delete_any(context: Context, data_dict: DataDict) -> ActionResult.PackageCollaboratorDelete:
+    '''Allow anoy to remove a collaborator from a dataset.
+    '''
+
+    model = context['model']
+
+    package_id, user_id = _get_or_bust(
+        data_dict,
+        ['id', 'user_id']
+    )
+
+    package = model.Package.get(package_id)
+    if not package:
+        raise NotFound(_('Package not found'))
+
+    user = model.User.get(user_id)
+    if not user:
+        raise NotFound(_('User not found'))
+
+    collaborator = model.Session.query(model.PackageMember).\
+        filter(model.PackageMember.package_id == package.id).\
+        filter(model.PackageMember.user_id == user.id).one_or_none()
+    if not collaborator:
+        raise NotFound(
+            'User {} is not a collaborator on this package'.format(user_id))
+
+    model.Session.delete(collaborator)
+    model.repo.commit()
+
+    log.info('User {} removed as collaborator from package {}'.format(
+        user_id, package.id))
+    
 
 class CdpPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -125,7 +213,7 @@ class CdpPlugin(plugins.SingletonPlugin):
             'capacity': 'editor'   # ระดับสิทธิ์ของ collaborator
         }
         # เรียก action เพื่อเพิ่ม collaborator เข้าไปใน dataset; ผลลัพธ์จะถูกเก็บใน result_create
-        result_create = package_collaborator_create(context.copy(), data_create)
+        result_create = package_collaborator_create_any(context.copy(), data_create)
 
         # ตรวจสอบว่าค่า res_dict.get('data_cdp') ไม่ใช่ 'yes'
         if res_dict.get('data_cdp') != 'yes':
@@ -135,7 +223,7 @@ class CdpPlugin(plugins.SingletonPlugin):
                 'user_id': user_id     # ID ของ user ที่จะถูกลบออกจากการเป็น collaborator
             }
             # เรียก action เพื่อลบ collaborator ออกจาก dataset; ผลลัพธ์จะถูกเก็บใน result_delete
-            result_delete = package_collaborator_delete(context.copy(), data_delete)
+            result_delete = package_collaborator_delete_any(context.copy(), data_delete)
 
         # คืนค่า dictionary ผลลัพธ์ของการสร้าง dataset กลับไป
         return res_dict
@@ -159,7 +247,7 @@ class CdpPlugin(plugins.SingletonPlugin):
             'capacity': 'editor'   # ระดับสิทธิ์ของ collaborator
         }
         # เรียก action เพื่อเพิ่ม collaborator เข้าไปใน dataset; ผลลัพธ์จะถูกเก็บใน result_create
-        result_create = package_collaborator_create(context, data_create)
+        result_create = package_collaborator_create_any(context, data_create)
 
         # ตรวจสอบว่าค่า pkg_dict.get('data_cdp') ไม่ใช่ 'yes'
         if pkg_dict.get('data_cdp') != 'yes':
@@ -169,7 +257,7 @@ class CdpPlugin(plugins.SingletonPlugin):
                 'user_id': user_id     # ID ของ user ที่จะถูกลบออกจากการเป็น collaborator
             }
             # เรียก action เพื่อลบ collaborator ออกจาก dataset; ผลลัพธ์จะถูกเก็บใน result_delete
-            result_delete = package_collaborator_delete(context, data_delete)
+            result_delete = package_collaborator_delete_any(context, data_delete)
 
         # คืนค่า dictionary ข้อมูล dataset ที่อัปเดตแล้วกลับไป
         return pkg_dict
